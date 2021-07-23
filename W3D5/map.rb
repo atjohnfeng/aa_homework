8513880cf7100277c614461c487aab3a69a0f97b
atjohnfeng <atjohnfeng@gmail.com>
2021-07-22 23:24:20 -0700

    completed map class

---
+++ W3D5/map.rb
@@ -0,0 +1,37 @@
+class Map
+
+    def initialize
+        @map = []
+    end
+
+    def set(key, value)
+        @map.each_with_index do |defin|
+            if defin[0] == key
+                defin[1] == value
+                return true
+            end
+        end
+        @map << [key, value]
+    end
+
+    def get(key)
+        @map.each_with_index do |defin,i|
+            if defin[0] == key
+                return @map[i][1]
+            end
+        end
+    end
+
+    def delete(key)
+        @map.each_with_index do |defin,i|
+            if defin[0] == key
+                @map.delete_at(i)
+            end
+        end
+    end
+
+    def show
+        @map
+    end
+
+end
