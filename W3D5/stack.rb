5221aa16a1a1e99c4acf3b042bad0cb6831ecb2f
atjohnfeng <atjohnfeng@gmail.com>
2021-07-22 23:04:49 -0700

    completed stack class

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,7 +1,19 @@
 class Stack
 
     def initialize
+        @stack = Array.new
+    end
 
+    def push(ele)
+        @stack.push(ele)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
     end
 
 end
