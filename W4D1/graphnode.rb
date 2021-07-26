fcf3603361344cbb0469462dbab8c742945eea72
atjohnfeng <atjohnfeng@gmail.com>
2021-07-25 22:58:55 -0700

    Added initialize and bfs methods

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -1,5 +1,39 @@
 class GraphNode
+    attr_reader :value
+    attr_accessor :neighbors
 
-    
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+    def self.bfs(starting_node, target)
+        queue = [starting_node]
+        checked = []
+        until queue.empty?
+            check_node = queue.shift
+            break if checked.include?(check_node)
+            checked << check_node
+            return true if check_node.value == target
+            check_node.neighbors.each do |node|
+                queue << node
+            end
+        end
+        return nil
+    end
 
 end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+p GraphNode.bfs(a, "b")
+p GraphNode.bfs(a, "f")
