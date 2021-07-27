e5cb20270443130135a53b61dd3104e8036cc10a
atjohnfeng <atjohnfeng@gmail.com>
2021-07-26 23:18:58 -0700

    Completed additional methods

--- W4D2/skeleton/lib/board.rb
+++ W4D2/skeleton/lib/board.rb
@@ -10,16 +10,29 @@ class Board
 
   def place_stones
     (0..12).each do |i|
-      if i != 6
-        4.times { @cups[i] << :stone }
-      end
+      4.times { @cups[i] << :stone } if i != 6
     end
   end
 
   def valid_move?(start_pos)
+    valid = [0,1,2,3,4,5,7,8,9,10,11,12]
+    raise 'Invalid starting cup' if !valid.include?(start_pos)
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
+    return true
   end
 
   def make_move(start_pos, current_player_name)
+    if valid_move?(start_pos)
+      stones = @cups[start_pos].length
+      @cups[start_pos] = []
+      ending_cup_idx = start_pos
+      (1..stones).each do |i|
+        @cups[start_pos+i] << :stone
+        ending_cup_idx = start_pos+i
+      end
+    end
+      next_turn(ending_cup_idx)
+      render
   end
 
   def next_turn(ending_cup_idx)
@@ -35,9 +48,10 @@ class Board
   end
 
   def one_side_empty?
-    @board[1..6].all? { |cup| cup.count == 0 } || @board[7..12].all? { |cup| cup.count == 0 }
+    @cups[1..6].all? { |cup| cup.count == 0 } || @cups[7..12].all? { |cup| cup.count == 0 }
   end
 
   def winner
+    one_side_empty?
   end
 end
