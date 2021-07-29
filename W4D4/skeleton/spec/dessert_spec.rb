91408cd6343ebbea3422ab68bda3c1df08f725d7
atjohnfeng <atjohnfeng@gmail.com>
2021-07-28 22:41:20 -0700

    Implemented Dessert class RSpecs

--- W4D4/skeleton/spec/dessert_spec.rb
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -1,42 +1,54 @@
 require 'rspec'
 require 'dessert'
 
-=begin
-Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
-=end
-
 describe Dessert do
+
+  subject(:dessert) { Dessert.new('Ice Cream', 1, 'chef') }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(dessert.type).to eq("Ice Cream")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(1)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq ([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Ice Cream", "Non-integer", "chef")}.to raise_error(ArgumentError)
+    end
+
   end
 
-  describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
-  end
+  # describe "#add_ingredient" do
 
-  describe "#mix!" do
-    it "shuffles the ingredient array"
-  end
+  #   it "adds an ingredient to the ingredients array" do
+    
+  #   end
 
-  describe "#eat" do
-    it "subtracts an amount from the quantity"
+  # end
 
-    it "raises an error if the amount is greater than the quantity"
-  end
+  # describe "#mix!" do
+  #   it "shuffles the ingredient array"
+  # end
 
-  describe "#serve" do
-    it "contains the titleized version of the chef's name"
-  end
+  # describe "#eat" do
+  #   it "subtracts an amount from the quantity"
 
-  describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
-  end
+  #   it "raises an error if the amount is greater than the quantity"
+  # end
+
+  # describe "#serve" do
+  #   it "contains the titleized version of the chef's name"
+  # end
+
+  # describe "#make_more" do
+  #   it "calls bake on the dessert's chef with the dessert passed in"
+  # end
+
 end
