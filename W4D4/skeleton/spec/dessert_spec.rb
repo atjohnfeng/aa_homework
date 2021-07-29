9432e3a150be4fcdc5ff02373721712c162abd0d
atjohnfeng <atjohnfeng@gmail.com>
2021-07-28 22:56:12 -0700

    Completed eat rspec

--- W4D4/skeleton/spec/dessert_spec.rb
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -7,6 +7,7 @@ describe Dessert do
   let(:chef) { double("chef") }
 
   describe "#initialize" do
+
     it "sets a type" do
       expect(dessert.type).to eq("Ice Cream")
     end
@@ -25,30 +26,54 @@ describe Dessert do
 
   end
 
-  # describe "#add_ingredient" do
+  describe "#add_ingredient" do
 
-  #   it "adds an ingredient to the ingredients array" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient('ice')
+
+      expect(dessert.ingredients).to eq(['ice'])
+    end
+
+  end
+
+  describe "#mix!" do
+  
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient('ice')
+      dessert.add_ingredient('cream')
+      dessert.mix!
+
+      expect(dessert).to receive(:shuffle!).with(ingredients)
+    end
+
+  end
+
+  describe "#eat" do
+
+    it "subtracts an amount from the quantity" do
+      dessert.eat(1)
+
+      expect(dessert.quantity).to eq(0)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect do
+        dessert.eat(2)
+      end.to raise_error("not enough left!")
+    end
+
+  end
+
+  describe "#serve" do
+
+    it "contains the titleized version of the chef's name"
     
-  #   end
+  end
 
-  # end
+  describe "#make_more" do
 
-  # describe "#mix!" do
-  #   it "shuffles the ingredient array"
-  # end
+    it "calls bake on the dessert's chef with the dessert passed in"
 
-  # describe "#eat" do
-  #   it "subtracts an amount from the quantity"
-
-  #   it "raises an error if the amount is greater than the quantity"
-  # end
-
-  # describe "#serve" do
-  #   it "contains the titleized version of the chef's name"
-  # end
-
-  # describe "#make_more" do
-  #   it "calls bake on the dessert's chef with the dessert passed in"
-  # end
+  end
 
 end
