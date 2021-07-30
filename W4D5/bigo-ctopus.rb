d7f7dd44b31077169c81fbb5ef911f40e4f8c91c
atjohnfeng <atjohnfeng@gmail.com>
2021-07-29 23:08:46 -0700

    Added Dancing Octopi

--- W4D5/bigo-ctopus.rb
+++ W4D5/bigo-ctopus.rb
@@ -46,4 +46,30 @@ p sluggish_octopus(fishies)
 p dominant_octopus(fishies)[-1]
 p clever_octopus(fishies)
 
+#Linear
+def slow_dance(direction, tiles_array)
+    tiles_array.each { |move| return tiles_array.index(move) if move == direction }
+end
+
+#Constant
+def fast_dance(direction, tiles_structure)
+    return tiles_structure[direction]
+end
+
 tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+new_tiles_data_structure = { 
+    "up" => 0, 
+    "right-up" => 1, 
+    "right" => 2, 
+    "right-down" => 3, 
+    "down" => 4, 
+    "left-down" => 5, 
+    "left" => 6,  
+    "left-up" => 7
+}
+
+p slow_dance("up", tiles_array)
+p slow_dance("right-down", tiles_array)
+p fast_dance("up", new_tiles_data_structure)
+p fast_dance("right-down", new_tiles_data_structure)
