1f062d67293046f7265a0fbef6743d36b3609c99
atjohnfeng <atjohnfeng@gmail.com>
2021-07-29 22:38:52 -0700

    Added clever_octopus method

--- W4D5/bigo-ctopus.rb
+++ W4D5/bigo-ctopus.rb
@@ -1,9 +1,29 @@
-fishies = [
-    'fish', 
-    'fiiish', 
-    'fiiiiish', 
-    'fiiiish', 
-    'fffish', 
-    'ffiiiiisshh', 
-    'fsh', 
-    'fiiiissshhhhhh']
+class Octopus
+
+    FISHIES = [
+        'fish', 
+        'fiiish', 
+        'fiiiiish', 
+        'fiiiish', 
+        'fffish', 
+        'ffiiiiisshh', 
+        'fsh', 
+        'fiiiissshhhhhh']
+
+    def self.sluggish_octopus
+        
+    end
+
+    def self.dominant_octopus
+
+    end
+
+    def self.clever_octopus
+        longest_fish = FISHIES[0]
+        FISHIES.each_with_index { |fish,i| longest_fish = FISHIES[i] if fish.length > longest_fish.length }
+        longest_fish
+    end
+
+end
+
+p Octopus.clever_octopus
