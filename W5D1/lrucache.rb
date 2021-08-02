09da0eb4b2a31ad6a082938646f9731ed56813c9
atjohnfeng <atjohnfeng@gmail.com>
2021-08-02 13:32:11 -0700

    Additional changes

--- W5D1/lrucache.rb
+++ W5D1/lrucache.rb
@@ -9,14 +9,14 @@ class LRUCache
     end
 
     def add(el)
-      if count == size && !@cache.include?(el)
+      if count == @size && !@cache.include?(el)
         @cache.unshift(el)
         @cache.push(el)
       elsif @cache.include?(el)
         @cache.delete(el)
         @cache.push(el)
       else
-        @cach.push(el)
+        @cache.push(el)
       end
     end
 
@@ -47,4 +47,4 @@ johnny_cache.add("I walk the line")
 johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
