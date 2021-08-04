63a4249d07e40244a9df57da8228b8e5f3429e9f
atjohnfeng <atjohnfeng@gmail.com>
2021-08-04 09:45:17 -0700

    Fix find by methods

--- W5D3/skeleton/plays.rb
+++ W5D3/skeleton/plays.rb
@@ -61,7 +61,7 @@ class Play
   end
 
   def find_by_playwright(playwright)
-    PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+    PlayDBConnection.instance.execute(<<-SQL, playwright_id)
       SELECT
         *
       FROM
