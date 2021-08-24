9e47fe751b00f37e00c40a119a825a3c96166fe2
atjohnfeng <atjohnfeng@gmail.com>
2021-08-23 23:53:29 -0700

    Complete homework problems

--- W8D2/problems.js
+++ W8D2/problems.js
@@ -16,17 +16,52 @@
 // isSubstring("hello world", "world");
 // isSubstring("hello world", "olleh");
 
-function fizzBuzz(array) {
+// function fizzBuzz(array) {
 
-    let fizzy = [];
+//     let fizzy = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if ((array[i] % 3 === 0 || array[i] % 5 === 0) && !(array[i] % 3 === 0 && array[i] % 5 === 0)) {
-            fizzy.push(array[i]);
+//     for (let i = 0; i < array.length; i++) {
+//         if ((array[i] % 3 === 0 || array[i] % 5 === 0) && !(array[i] % 3 === 0 && array[i] % 5 === 0)) {
+//             fizzy.push(array[i]);
+//         }
+//     }
+
+//     console.log(fizzy);
+// }
+
+// fizzBuzz([1,2,3,4,5,15]);
+
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
         }
     }
-
-    console.log(fizzy);
+    return num !== 1 && num !== 0;
 }
 
-fizzBuzz([1,2,3,4,5,15]);
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+
+function sumOfNPrimes(num) {
+
+    let sum = 0;
+    let i = 0;
+    let x = 2;
+
+    while (i < num) {
+        if (isPrime(x)) {
+            sum += x;
+            i++;
+        }
+        x++;
+    }
+
+    return sum;
+
+}
+
+console.log(sumOfNPrimes(0))
+console.log(sumOfNPrimes(1))
+console.log(sumOfNPrimes(4))
