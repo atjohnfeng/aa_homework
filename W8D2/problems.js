1f26aff7e1fc7b6040fb4bcc00a9caa8704a9666
atjohnfeng <atjohnfeng@gmail.com>
2021-08-23 23:16:58 -0700

    Add madLib problem

---
+++ W8D2/problems.js
@@ -0,0 +1,10 @@
+function madLib(verb, adj, noun) {
+
+    let a = verb.toUpperCase();
+    let b = adj.toUpperCase();
+    let c = noun.toUpperCase();
+
+    console.log(`We shall ${a} the ${b} ${c}.`);
+}
+
+madLib('make', 'best', 'guac');
