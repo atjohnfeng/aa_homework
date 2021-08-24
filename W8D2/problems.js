a8e8581db29c0c382fbac03effc1118ebcbbdb45
atjohnfeng <atjohnfeng@gmail.com>
2021-08-23 23:21:39 -0700

    Add isSubstring problem

--- W8D2/problems.js
+++ W8D2/problems.js
@@ -1,10 +1,17 @@
-function madLib(verb, adj, noun) {
+// function madLib(verb, adj, noun) {
 
-    let a = verb.toUpperCase();
-    let b = adj.toUpperCase();
-    let c = noun.toUpperCase();
+//     let a = verb.toUpperCase();
+//     let b = adj.toUpperCase();
+//     let c = noun.toUpperCase();
 
-    console.log(`We shall ${a} the ${b} ${c}.`);
+//     console.log(`We shall ${a} the ${b} ${c}.`);
+// }
+
+// madLib('make', 'best', 'guac');
+
+function isSubstring(searchString, subString) {
+    console.log(searchString.includes(subString));
 }
 
-madLib('make', 'best', 'guac');
+isSubstring("hello world", "world");
+isSubstring("hello world", "olleh");
